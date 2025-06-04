0f2e3a74baba870d8648767f0fa2d8eee48d4629
Felix Romero <ferofern@espol.edu.ec>
2025-06-04 15:35:03 +0000

    commit prueba

---
+++ src/js/file01.js
@@ -0,0 +1,8 @@
+"use strict";
+
+// Función de autoejecución para mostrar mensaje de bienvenida
+(function() {
+    const mensaje = "¡Bienvenido a la página!";
+    alert(mensaje);
+    console.log(mensaje);
+})();
